1ddc93532a44aab8696051b7e9cef2395a7eeb27
Arnab <arnabdatta3@gmail.com>
2020-07-01 23:44:22 -0400

    comment added

--- FileSplitter/src/FileSplitter.java
+++ FileSplitter/src/FileSplitter.java
@@ -2,7 +2,9 @@
 public class FileSplitter {
 
 	public static void main(String args[]) {
+		//Get the file name from command. error handling required
 		String fileName = args[0];
+		//Get the Split at number
 		int splitAt = Integer.parseInt(args[1]);
 		FileSplitterProcess fileSplitterProcess = new FileSplitterProcess();
 		if (fileSplitterProcess.loadFile(fileName)) {
